86fead41be77936aa855da779146ca05a966e87e
EnginYıldırım <enginyldrm02@gmail.com>
2023-09-14 11:19:59 +0200

    * Instruments and Post Types added to the tables as initial
    * AWS RDS MS SQL connection string added to the configuration

--- Data/Migrations/20230914082405_Instruments_And_PostTypes_Initilization.Designer.cs
+++ Data/Migrations/20230914082405_Instruments_And_PostTypes_Initilization.Designer.cs
@@ -4,6 +4,7 @@ using EternalBAND.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EternalBAND.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230914082405_Instruments_And_PostTypes_Initilization")]
+    partial class Instruments_And_PostTypes_Initilization
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
