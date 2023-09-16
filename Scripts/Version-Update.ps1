85a853b4c38bb9028321500efed22a23af20ddd9
EnginYıldırım <enginyldrm02@gmail.com>
2023-09-16 22:23:51 +0200

    versioning addition test

---
+++ Scripts/Version-Update.ps1
@@ -0,0 +1,4 @@
+﻿$version = [System.Reflection.Assembly]::GetExecutingAssembly().GetName().Version
+$newVersion = [System.Version]($version.Major, $version.Minor, ($version.Build + 1), 0)
+Write-Host "Updating version to $newVersion"
+(Get-Content -Path 'AssemblyInfo.cs') -replace '(\d+\.\d+\.\d+\.\d+)', "$newVersion" | Set-Content -Path 'AssemblyInfo.cs'
