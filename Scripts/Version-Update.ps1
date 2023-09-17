ebe5dd134c04a2ca7f6fb4f7157891312d7df94f
EnginYıldırım <enginyldrm02@gmail.com>
2023-09-17 08:59:59 +0200

    Ps1 file corrected to get correct version data

--- Scripts/Version-Update.ps1
+++ Scripts/Version-Update.ps1
@@ -1,4 +1,8 @@
-﻿$version = [System.Reflection.Assembly]::GetExecutingAssembly().GetName().Version
-$newVersion = [System.Version]($version.Major, $version.Minor, ($version.Build + 1), 0)
+﻿# $version = [System.Reflection.Assembly]::GetExecutingAssembly().GetName().Version
+$projectDirectory = Split-Path -Path $PSScriptRoot -Parent
+$assemblyPath = Join-Path -Path $projectDirectory -ChildPath "bin\Debug\net7.0\EternalBAND.dll"
+$assembly = [System.Reflection.Assembly]::LoadFrom($assemblyPath)
+$version = $assembly.GetName().Version
+$newVersion = [System.Version]::new($version.Major, $version.Minor, ($version.Build + 1), 0)
 Write-Host "Updating version to $newVersion"
-(Get-Content -Path 'AssemblyInfo.cs') -replace '(\d+\.\d+\.\d+\.\d+)', "$newVersion" | Set-Content -Path 'AssemblyInfo.cs'
+(Get-Content ..\Properties\AssemblyInfo.cs) -replace '(\d+\.\d+\.\d+\.\d+)', "$newVersion" | Set-Content -Path ..\Properties\AssemblyInfo.cs
