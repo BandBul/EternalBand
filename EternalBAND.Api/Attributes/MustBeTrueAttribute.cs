16e7a250ea0619be066b6eabea1d54f856579540
EnginYıldırım <enginyldrm02@gmail.com>
2024-04-07 21:15:59 +0200

    GDPR and Privacy Policy pages are added
    GDPR Checkbox introduced for registration page

---
+++ EternalBAND.Api/Attributes/MustBeTrueAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EternalBAND.Api.Attributes
+{
+    public class MustBeTrueAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            if (value is bool)
+            {
+                return (bool)value == true;
+            }
+
+            return false;
+        }
+    }
+}
