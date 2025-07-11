988495a8e8261fcb79684a77874a6915f89a6b5b
engin.yildirim <enginyldrm02@gmail.com>
2025-07-12 01:11:52 +0200

    AdminWeb Controller is converted to actual WebAPI
    initial filtering is introduced for jwt handling for webAPI calls

--- EternalBAND.Win/Middleware/JwtHandlingMiddleware.cs
+++ EternalBAND.Win/Middleware/JwtHandlingMiddleware.cs
@@ -2,6 +2,7 @@
 using EternalBAND.Common;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 using System.Net;
 using System.Security.Claims;
 
@@ -20,6 +21,8 @@ namespace EternalBAND.Win.Middleware
             _logger = logger;
         }
 
+        // TODO implement two jwt token check logic one for swagger/api it will check Authorization header
+        // second is for MVC/Razor requests which will be controlled by cookie ( delete/add/update cookie)
         public async Task InvokeAsync(HttpContext context)
         {
             var token = context.Request.Cookies[Constants.AccessTokenCookieName];
@@ -62,13 +65,27 @@ namespace EternalBAND.Win.Middleware
                 }
                 catch (Exception ex)
                 {
+                    context.Response.Cookies.Delete(Constants.AccessTokenCookieName);
                     _logger.LogError(ex, "Error parsing the JWT token.");
                     // If an error occurs (e.g., token is invalid), redirect to login
                     context.Response.Redirect($"/{UrlConstants.Login}");
                     return;
                 }
             }
+            else
+            {
+                var path = context.Request.Path.Value;
 
+                if (path.StartsWith("/api", StringComparison.OrdinalIgnoreCase))
+                {
+                    // Redirect to login page
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Unauthorized - Token missing.");
+                    return;
+                }
+                context.Response.Redirect($"/{UrlConstants.Login}");
+                return;
+            }
             // Continue processing if token is valid or does not exist
             await _next(context);
         }
