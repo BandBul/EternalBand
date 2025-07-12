cb3024a3a4404aefab53865caf0d51bb6ccc3581
engin.yildirim <enginyldrm02@gmail.com>
2025-07-12 02:06:57 +0200

    Token will be read from Authorization for web api , we can now use external FE apps to request our authorized data

--- EternalBAND.Win/Middleware/JwtHandlingMiddleware.cs
+++ EternalBAND.Win/Middleware/JwtHandlingMiddleware.cs
@@ -25,67 +25,90 @@ namespace EternalBAND.Win.Middleware
         // second is for MVC/Razor requests which will be controlled by cookie ( delete/add/update cookie)
         public async Task InvokeAsync(HttpContext context)
         {
-            var token = context.Request.Cookies[Constants.AccessTokenCookieName];
             var excludedPaths = new[] { UrlConstants.Login, UrlConstants.Logout };
-            var requestPath = context.Request.Path.Value?.ToLower();
-            if (excludedPaths.Any(p => requestPath != null && (requestPath.StartsWith(p) || requestPath.EndsWith(p))))
+            var requestPath = context.Request.Path;
+
+            if (excludedPaths.Any(p => requestPath != null && (requestPath.Value.StartsWith(p) || requestPath.Value.EndsWith(p))))
             {
                 await _next(context);
                 return;
             }
 
-            if (!string.IsNullOrEmpty(token))
+            var isApiRequest = requestPath.StartsWithSegments("/api") ||
+                            context.Request.Headers["Accept"].ToString().Contains("application/json");
+            var token = context.Request.Cookies[Constants.AccessTokenCookieName];
+
+            // WebAPI
+            if (isApiRequest)
             {
-                try
+                if (!context.Request.Headers.ContainsKey("Authorization") && !string.IsNullOrEmpty(token))
                 {
-                    var authService = context.RequestServices.GetRequiredService<IAuthenticationService>();
-                    // Validate JWT token
-                    var principal = authService.ValidateToken(token, out SecurityToken jwtToken);
-
-                    // Check if the token has expired
-                    //if (jwtToken.ValidTo < DateTime.UtcNow.AddSeconds(3570))
-                    if (jwtToken.ValidTo < DateTime.UtcNow)
-                    {
-                        _logger.LogInformation("JWT token expired, redirecting to login.");
-                        context.Response.Cookies.Delete(Constants.AccessTokenCookieName);
-                        context.User = new ClaimsPrincipal(new ClaimsIdentity());
-                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                        // Token is expired, redirect to the login page
-                        context.Response.Redirect($"/{UrlConstants.Login}");
-                        return; // Stop further processing
-                    }
-
-                    context.User = principal;
-
-                    //if (!context.Request.Headers.ContainsKey(HttpRequestHeader.Authorization.ToString()))
-                    //{
-                    //    context.Request.Headers[HttpRequestHeader.Authorization.ToString()] = $"Bearer {token}";
-                    //}
-
+                    context.Request.Headers["Authorization"] = $"Bearer {token}";
                 }
-                catch (Exception ex)
-                {
-                    context.Response.Cookies.Delete(Constants.AccessTokenCookieName);
-                    _logger.LogError(ex, "Error parsing the JWT token.");
-                    // If an error occurs (e.g., token is invalid), redirect to login
-                    context.Response.Redirect($"/{UrlConstants.Login}");
-                    return;
-                }
-            }
-            else
-            {
-                var path = context.Request.Path.Value;
 
-                if (path.StartsWith("/api", StringComparison.OrdinalIgnoreCase))
+                else if (context.Request.Headers.TryGetValue("Authorization", out var authHeader))
                 {
-                    // Redirect to login page
+                    var tokenString = authHeader.ToString();
+                    token = 
+                        tokenString.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase) ?
+                        tokenString.Substring("Bearer ".Length).Trim() :
+                        tokenString;
+                }
+                else
+                {
                     context.Response.StatusCode = StatusCodes.Status401Unauthorized;
                     await context.Response.WriteAsync("Unauthorized - Token missing.");
                     return;
                 }
+            }
+
+            // MVC
+            else
+            {
+                if (string.IsNullOrEmpty(token))
+                {
+                    context.Response.Redirect($"/{UrlConstants.Login}");
+                    return;
+                }
+            }
+
+            try
+            {
+
+                var authService = context.RequestServices.GetRequiredService<IAuthenticationService>();
+                // Validate JWT token
+                var principal = authService.ValidateToken(token, out SecurityToken jwtToken);
+
+                // Check if the token has expired
+                //if (jwtToken.ValidTo < DateTime.UtcNow.AddSeconds(3570))
+                if (jwtToken.ValidTo < DateTime.UtcNow)
+                {
+                    _logger.LogInformation("JWT token expired, redirecting to login.");
+                    context.Response.Cookies.Delete(Constants.AccessTokenCookieName);
+                    context.User = new ClaimsPrincipal(new ClaimsIdentity());
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    // Token is expired, redirect to the login page
+                    context.Response.Redirect($"/{UrlConstants.Login}");
+                    return; // Stop further processing
+                }
+
+                context.User = principal;
+            }
+            catch (Exception ex)
+            {
+                context.Response.Cookies.Delete(Constants.AccessTokenCookieName);
+                _logger.LogError(ex, "Error parsing the JWT token.");
+                if (isApiRequest)
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Invalid token");
+                    return;
+                }
+
                 context.Response.Redirect($"/{UrlConstants.Login}");
                 return;
             }
+
             // Continue processing if token is valid or does not exist
             await _next(context);
         }
